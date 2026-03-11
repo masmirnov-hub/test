58640773a50f00dadd8765a3f333eafeb03df2b9
masmirnov-hub <ma.smirnov01@mail.ru>
2026-03-12 02:14:42 +0300

    Create security_tests.py

---
+++ security_tests.py
@@ -0,0 +1,8 @@
+# security_tests.py
+API_KEY = "sk-1234567890abcdef"  # Hardcoded API key
+PASSWORD = "admin123"            # Hardcoded password
+
+subprocess.call(user_input, shell=True)  # Команда из пользователя
+pickle.loads(data)                        # Загрузка данных
+hashlib.md5(b"password")                   # Хеширование пароля
+eval(user_code)                            # Выполнение кода
